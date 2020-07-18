919b5622a04a5af815641880928a8b2eb2e89358
Robert Stevens <robertstevens2015@gmail.com>
2020-07-17 20:10:04 -0500

    All tasks have been completed, and I have a very good understanding of the unit!

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,6 +21,9 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach( function (array){
+  displayNames.push({display: `Name: ${array.animal_name}, Scientific Name: ${array.scientific_name}`})
+})
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -29,7 +32,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = zooAnimals.map( function(array) {
+  return array.animal_name;
+})
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -37,7 +42,9 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
+const lowPopulationAnimals = zooAnimals.filter( function (item) {
+  return item.population < 5
+})
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -45,7 +52,11 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
+
+const callback = function(total, toAdd) {
+  return total + toAdd.population;
+}
+let populationTotal = zooAnimals.reduce(callback, 0);
 console.log(populationTotal);
 
 
@@ -57,19 +68,33 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, cb){
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add (a, b){
+  let answer = a + b;
+  return answer;
+}
+function multiply(a, b){
+  let answer = a * b;
+  return answer;
+}
 
+function greeting(a, b){
+  let answer = `Hello ${a} ${b}, nice to meet you!`;
+  return answer;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,17 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: The nested function can access the variable internal because they are both declared within the same function. This means that since the nested function and the variable are declared in the same scope, it can be accessed by the functions in the same scope.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(numberGiven){
+  let sum = 0
+  for (let i = 0; i <= numberGiven; i++){
+    sum = sum + i;
+  }
+  return sum;
+}
+console.log(summation(4));
